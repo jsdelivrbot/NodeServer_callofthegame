5cf9a12ddd39f4062fbe09e60b42823b5b1a8fb1
Atlar <tuxoehebo@yandex.ru>
2018-06-08 10:51:05 +0300

    tutorial: try display challenge name var

---
+++ ChallengeModule/challenge_module.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+// Loads Challenge
+// STUB
+
+exports.load_challenge = load_challenge;
+
+
+function load_challenge(){
+    challenge_name = "First Super Challenge";
+    return challenge_name;
+};
+
