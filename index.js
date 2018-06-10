38c565794b6ca1a82341b47ada558203ba0953d7
Atlar <tuxoehebo@yandex.ru>
2018-06-10 17:18:10 +0300

    Added webpack and modules for React to work

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-var chall = require('./ChallngeModule/challenge_module')
+var chall = require('./ChallengeModule/challenge_module')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index', { challenge_name: chall.load_chalenge()}))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index', { challenge_name: chall.load_challenge() }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
